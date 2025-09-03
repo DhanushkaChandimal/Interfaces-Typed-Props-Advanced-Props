eb3a4f9113edd6134859e51d3f0617e7eaa63b97
Dhanushka <dhanushkamcr@gmail.com>
2025-09-03 18:59:10 -0500

    pass component as a prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import type { Person, ProfileProps } from './models/Person.model';
 import PersonComponent from './components/PersonComponent';
 import Status from './components/Status';
 import Header from './components/Header';
+import NestedComponent from './components/NestedComponent';
 import Card from 'react-bootstrap/Card';
 
 function App() {
@@ -28,6 +30,13 @@ function App() {
       <Status />
       <Header>Hey there buddy!</Header>
       <Header children={'children message'}/>
+
+      <NestedComponent>
+          <h3>This is a children component</h3>
+          <Status />
+      </NestedComponent>
+      <NestedComponent children={<Status />}/>
+
     </div>
   );
 }
